c0fa66faa8288ccf81ec3e5669b070e2580eb8d3
Parimal108 <84119397+Parimal108@users.noreply.github.com>
2021-05-13 21:39:27 +0530

    Add files via upload

---
+++ task1 product of 2 num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main()
+ 
+ { 
+ float x,y,z;
+ cout<<"Enter value of x "<<endl;
+ cin>>x;
+ cout<<"Enter value of y "<<endl;
+ cin>>y;
+ z=x*y;
+ cout<<"product of x and y::"<<z;
+  
+ 
+ }
