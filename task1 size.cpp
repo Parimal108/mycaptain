6cf4c0afb795c444b2c23aaa0dbc0e2e8f23a810
Parimal Dongare <84119397+Parimal108@users.noreply.github.com>
2021-05-15 01:28:11 +0530

    Add files via upload

---
+++ task1 size.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a;
+char b;
+float c;
+double d;
+
+cout<<"size of int::" <<sizeof(a)<<"\n";
+cout<<"size of char::" <<sizeof(b)<<"\n";
+cout<<"size of float::" <<sizeof(c)<<"\n";
+cout<<"size of double::" <<sizeof(d)<<"\n";
+}
