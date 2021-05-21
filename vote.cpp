73ddcde249b9dcc31b59558cc3fb0b9e65493201
Parimal Dongare <84119397+Parimal108@users.noreply.github.com>
2021-05-21 12:14:33 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int age;
+cout<<"Enter your Age:: ";
+cin>>age;
+
+ if(age>=18)
+  { 
+  cout<<"\nyou are eligible for voting";
+  
+  }
+  
+  else
+    cout<<"\nyou are not eligible for voting";
+    
+    return 0;
+}
